690d6d7c945d1b92a18d7592bad2a729e701dde1
John Martin <john@lonepixel.com>
2015-08-21 12:07:39 -0700

    Add environment-specific shell scripts to initialize the shell environment.

---
+++ conf/local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+IVETL_LOCAL=1
+export IVETL_LOCAL
+
+unset IVETL_QA
+unset IVETL_PROD
+
+CQLSH=/Users/john/Projects/dse/bin/cqlsh
+export CQLSH
---
+++ conf/qa.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+IVETL_QA=1
+export IVETL_QA
+
+unset IVETL_LOCAL
+unset IVETL_PROD
