50d27c49b4e52dae39d65daa873c22d48e614353
John Martin <john@lonepixel.com>
2015-09-24 13:37:34 -0600

    Add a publisher name col.

--- ivetl/models/PublisherMetadata.py
+++ ivetl/models/PublisherMetadata.py
@@ -4,6 +4,7 @@ from cassandra.cqlengine.models import Model
 
 class Publisher_Metadata(Model):
     publisher_id = columns.Text(primary_key=True)
+    name = columns.Text()
     hw_addl_metadata_available = columns.Boolean()
     issn_to_hw_journal_code = columns.Map(columns.Text(), columns.Text())
     published_articles_issns_to_lookup = columns.List(columns.Text())
@@ -12,3 +13,7 @@ class Publisher_Metadata(Model):
     crossref_username = columns.Text()
     crossref_password = columns.Text()
     supported_pipelines = columns.List(columns.Text(index=True))
+
+    @property
+    def display_name(self):
+        return self.name or self.publisher_id
