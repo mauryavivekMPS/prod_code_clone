2a86ec4242a6cc65142ae3dd26bdd86138be726d
nmehta <nmehta@highwire.stanford.edu>
2015-10-13 12:36:52 -0700

    For flattening out citation_sources to separate fields in cf

--- deploy/schema/1.3/03_migrate_citation_sources.py
+++ deploy/schema/1.3/03_migrate_citation_sources.py
@@ -28,22 +28,37 @@ class Article_Citations(Model):
     created = columns.DateTime()
     updated = columns.DateTime()
 
+class Publisher_Metadata(Model):
+    publisher_id = columns.Text(primary_key=True)
+    hw_addl_metadata_available = columns.Boolean()
+    issn_to_hw_journal_code = columns.Map(columns.Text(), columns.Text())
+    published_articles_issns_to_lookup = columns.List(columns.Text())
+    published_articles_last_updated = columns.DateTime()
+    scopus_api_keys = columns.List(columns.Text())
+    crossref_username = columns.Text()
+    crossref_password = columns.Text()
+
 
 if __name__ == "__main__":
     open_cassandra_connection()
 
-    ctr = 0
-    for citation in Article_Citations.objects.limit(5000000):
+    publishers = Publisher_Metadata.objects.limit(1000)
 
-        ctr += 1
-        print("Updating #" + ctr)
-        
-        if citation.citation_sources:
-            if 'Scopus' in citation.citation_sources:
-                citation.citation_source_scopus = True
+    for p in publishers:
 
-            if 'Crossref' in citation.citation_sources:
-                citation.citation_source_xref = True
+        if p.publisher_id.endswith('cohort'):
+            continue
 
-            citation.save()
+        print("Updating publisher: " + p.publisher_id)
+        for citation in Article_Citations.objects.filter(publisher_id=p.publisher_id).limit(5000000):
+
+            print('.')
+            if citation.citation_sources:
+                if 'Scopus' in citation.citation_sources:
+                    citation.citation_source_scopus = True
+
+                if 'Crossref' in citation.citation_sources:
+                    citation.citation_source_xref = True
+
+                citation.save()
     close_cassandra_connection()
