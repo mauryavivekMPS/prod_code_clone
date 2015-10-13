d9f50249b679fec2fd61b16f55c90b1a8adac37f
John Martin <john@lonepixel.com>
2015-10-13 12:38:56 -0700

    Add proper IVETL_ROOT.

--- conf/prod.sh
+++ conf/prod.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-IVETL_ROOT=/iv/
-export IVETL_ROOT
-
 IVETL_PROD=1
 export IVETL_PROD
 
 unset IVETL_LOCAL
 unset IVETL_QA
+
+IVETL_ROOT=/iv/impactvizor-pipeline
+export IVETL_ROOT
 
 IVETL_CASSANDRA_IP=10.0.1.12
 export IVETL_CASSANDRA_IP
