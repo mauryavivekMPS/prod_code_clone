3e9f9e0e997570ed1413df12f709ef67ec998c6d
John Martin <john@lonepixel.com>
2015-11-02 19:59:20 -0800

    Removed per-developer settings from shared local.py.

--- conf/local.sh
+++ conf/local.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-IVETL_ROOT=/Users/nmehta/git/impactvizor-pipeline
-export IVETL_ROOT
-
 IVETL_LOCAL=1
 export IVETL_LOCAL
 
 unset IVETL_QA
 unset IVETL_PROD
-
-CQLSH=/Users/john/Projects/dse/bin/cqlsh
-export CQLSH
 
 DJANGO_SETTINGS_MODULE=ivweb.settings.local
 export DJANGO_SETTINGS_MODULE
