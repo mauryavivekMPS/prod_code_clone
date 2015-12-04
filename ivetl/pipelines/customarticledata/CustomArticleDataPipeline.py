aed607f6f204e46bfcfc38b22cfb740066410f06
John Martin <john@lonepixel.com>
2015-12-04 01:25:34 -0800

    Added explicit file list option to the file upload pipelines and made use of it from the FTP server. Also added the success notification email to rejected articles. Also fixed a bug in the ftp server.

--- ivetl/pipelines/customarticledata/CustomArticleDataPipeline.py
+++ ivetl/pipelines/customarticledata/CustomArticleDataPipeline.py
@@ -18,7 +18,7 @@ class CustomArticleDataPipeline(Pipeline):
     # 4. InsertArticleData - insert non-overlapping data into pub_articles and overlapping into _values.
     # 5. ResolveArticleData - decide which data to promote from _values into pub_articles, and do the insert.
 
-    def run(self, publisher_id_list=[], product_id=None, preserve_incoming_files=False, alt_incoming_dir=None, initiating_user_email=None):
+    def run(self, publisher_id_list=[], product_id=None, preserve_incoming_files=False, alt_incoming_dir=None, files=[], initiating_user_email=None):
         pipeline_id = 'custom_article_data'
 
         now = datetime.datetime.now()
@@ -33,62 +33,57 @@ class CustomArticleDataPipeline(Pipeline):
         else:
             publishers = Publisher_Metadata.objects.all()
 
-        if alt_incoming_dir:
-            base_incoming_dir = alt_incoming_dir
-        else:
-            base_incoming_dir = common.BASE_INCOMING_DIR
-
         # figure out which publisher has a non-empty incoming dir
         for publisher in publishers:
 
             if product['cohort']:
                 continue
 
-            publisher_dir = self.get_incoming_dir_for_publisher(base_incoming_dir, publisher.publisher_id, pipeline_id)
-            if os.path.isdir(publisher_dir):
+            if not files:
+                if alt_incoming_dir:
+                    base_incoming_dir = alt_incoming_dir
+                else:
+                    base_incoming_dir = common.BASE_INCOMING_DIR
+
+                publisher_dir = self.get_or_create_incoming_dir_for_publisher(base_incoming_dir, publisher.publisher_id, pipeline_id)
 
                 # grab all files from the directory
                 files = [f for f in os.listdir(publisher_dir) if os.path.isfile(os.path.join(publisher_dir, f))]
 
                 # remove any hidden files, in particular .DS_Store
-                files = [f for f in files if not f.startswith('.')]
+                files = [os.path.join(publisher_dir, f) for f in files if not f.startswith('.')]
 
-                # create work folder, signal the start of the pipeline
-                work_folder = self.get_work_folder(today_label, publisher.publisher_id, product_id, pipeline_id, job_id)
-                self.on_pipeline_started(publisher.publisher_id, product_id, pipeline_id, job_id, work_folder, initiating_user_email=initiating_user_email, total_task_count=4, current_task_count=0)
+            # create work folder, signal the start of the pipeline
+            work_folder = self.get_work_folder(today_label, publisher.publisher_id, product_id, pipeline_id, job_id)
+            self.on_pipeline_started(publisher.publisher_id, product_id, pipeline_id, job_id, work_folder, initiating_user_email=initiating_user_email, total_task_count=4, current_task_count=0)
 
-                if files:
-                    # construct the first task args with all of the standard bits + the list of files
-                    task_args = {
-                        'publisher_id': publisher.publisher_id,
-                        'product_id': product_id,
-                        'pipeline_id': pipeline_id,
-                        'work_folder': work_folder,
-                        'job_id': job_id,
-                        'uploaded_files': [os.path.join(publisher_dir, f) for f in files],
-                        'preserve_incoming_files': preserve_incoming_files,
-                    }
+            if files:
+                # construct the first task args with all of the standard bits + the list of files
+                task_args = {
+                    'publisher_id': publisher.publisher_id,
+                    'product_id': product_id,
+                    'pipeline_id': pipeline_id,
+                    'work_folder': work_folder,
+                    'job_id': job_id,
+                    'uploaded_files': files,
+                    'preserve_incoming_files': preserve_incoming_files,
+                }
 
-                    # send alert email that we're processing for this publisher
-                    subject = "%s - %s - Processing started for: %s" % (pipeline_id, today_label, publisher_dir)
-                    text = "Processing files for " + publisher_dir
-                    common.send_email(subject, text)
+                # and run the pipeline!
+                chain(
+                    tasks.GetArticleDataFiles.s(task_args) |
+                    tasks.ValidateArticleDataFiles.s() |
+                    tasks.InsertCustomArticleDataIntoCassandra.s() |
+                    published_articles_tasks.ResolvePublishedArticlesData.s()
+                ).delay()
 
-                    # and run the pipeline!
-                    chain(
-                        tasks.GetArticleDataFiles.s(task_args) |
-                        tasks.ValidateArticleDataFiles.s() |
-                        tasks.InsertCustomArticleDataIntoCassandra.s() |
-                        published_articles_tasks.ResolvePublishedArticlesData.s()
-                    ).delay()
-
-                else:
-                    # note: this is annoyingly duplicated from task.pipeline_ended ... this should be factored better
-                    end_date = datetime.datetime.today()
-                    p = Pipeline_Status().objects.filter(publisher_id=publisher.publisher_id, product_id=product_id, pipeline_id=pipeline_id, job_id=job_id).first()
-                    if p is not None:
-                        p.end_time = end_date
-                        p.duration_seconds = (end_date - p.start_time).total_seconds()
-                        p.status = self.PL_COMPLETED
-                        p.updated = end_date
-                        p.update()
+            else:
+                # note: this is annoyingly duplicated from task.pipeline_ended ... this should be factored better
+                end_date = datetime.datetime.today()
+                p = Pipeline_Status().objects.filter(publisher_id=publisher.publisher_id, product_id=product_id, pipeline_id=pipeline_id, job_id=job_id).first()
+                if p is not None:
+                    p.end_time = end_date
+                    p.duration_seconds = (end_date - p.start_time).total_seconds()
+                    p.status = self.PL_COMPLETED
+                    p.updated = end_date
+                    p.update()
