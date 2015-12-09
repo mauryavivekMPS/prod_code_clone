ff02b0df45bbe4821857ff7f4be2fd3cfe7d3959
John Martin <john@lonepixel.com>
2015-12-09 12:26:54 -0800

    Add (sadly duplicated) tableau env vars to wsgi scripts.

--- ivweb/wsgi_prod.py
+++ ivweb/wsgi_prod.py
@@ -21,7 +21,12 @@ import os
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "bloop.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ivweb.settings.prod")
+
+# TODO: these should not be duplicated from the conf.sh files, somehow...
 os.environ["IVETL_ROOT"] = "/iv/impactvizor-pipeline"
+os.environ["IVETL_TABLEAU_SERVER"] = "vizors.org"
+os.environ["IVETL_TABLEAU_USERNAME"] = "nmehta"
+os.environ["IVETL_TABLEAU_PASSWORD"] = "Reena,1275"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
