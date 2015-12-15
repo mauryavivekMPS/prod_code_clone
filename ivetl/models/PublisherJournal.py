5e82a2b75e29a44dd6d82c7c3a3d0ce57ef0a7bc
John Martin <john@lonepixel.com>
2015-12-15 13:25:49 -0800

    Add uuids to publisher_user and publisher_journal join tables to prevent weirdness when deleting/creating/recreating.

--- ivetl/models/PublisherJournal.py
+++ ivetl/models/PublisherJournal.py
@@ -1,3 +1,4 @@
+import uuid
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
@@ -5,6 +6,7 @@ from cassandra.cqlengine.models import Model
 class Publisher_Journal(Model):
     publisher_id = columns.Text(primary_key=True)
     product_id = columns.Text(primary_key=True)
-    electronic_issn = columns.Text(primary_key=True)
+    uid = columns.UUID(primary_key=True, default=uuid.uuid4)
+    electronic_issn = columns.Text()
     print_issn = columns.Text()
     journal_code = columns.Text()
