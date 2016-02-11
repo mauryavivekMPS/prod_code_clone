8370101af4680df72223bd357308c922fe916f54
nmehta <nmehta@highwire.stanford.edu>
2016-02-11 12:35:12 -0800

    changing key to 'preprint_doi'

--- ivetl/pipelines/rejectedarticles/tasks/SelectPublishedArticleTask.py
+++ ivetl/pipelines/rejectedarticles/tasks/SelectPublishedArticleTask.py
@@ -42,7 +42,7 @@ class SelectPublishedArticleTask(Task):
                     xref_matches = xref_search_results_json['message']['items']
                     if 'preprint_doi' in data and data['preprint_doi'] is not None and data['preprint_doi'] != '':
                         tlogger.info("This manuscript has a preprint doi.")
-                        xref_matches = self.filter_out_published_doi(data['published_doi'], xref_matches)
+                        xref_matches = self.filter_out_published_doi(data['preprint_doi'], xref_matches)
 
                     xref_article = JournalFilterCheck.check(xref_matches, tlogger)
 
