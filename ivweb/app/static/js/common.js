150470809c1d1130405a79e3a034fa8f62514bd3
John Martin <john@lonepixel.com>
2016-02-16 14:29:52 -0800

    Disabled the submit for processing button when there are no validated files.

--- ivweb/app/static/js/common.js
+++ ivweb/app/static/js/common.js
@@ -407,6 +407,51 @@ var ListDemosPage = (function() {
 
 
 //
+// Pending files form
+//
+
+var PendingFilesForm = (function() {
+    var isDemo;
+
+    var enableSubmitForProcessing = function() {
+        $('.submit-for-processing-button').removeClass('disabled').prop('disabled', false);
+    };
+
+    var disableSubmitForProcessing = function() {
+        $('.submit-for-processing-button').addClass('disabled').prop('disabled', true);
+    };
+
+    var updateSubmitForProcessing = function() {
+        if (!isDemo) {
+            var table = $('table.all-files-table');
+            if (table.find('> tbody > tr.validated-file').length > 0) {
+                enableSubmitForProcessing();
+            }
+            else {
+                disableSubmitForProcessing();
+            }
+        }
+    };
+
+    var init = function(options) {
+        options = $.extend({
+            isDemo: false
+        }, options);
+
+        isDemo = options.isDemo;
+
+        updateSubmitForProcessing();
+    };
+
+    return {
+        updateSubmitForProcessing: updateSubmitForProcessing,
+        init: init
+    };
+
+})();
+
+
+//
 // File upload widget
 //
 
@@ -451,6 +496,7 @@ var FileUploadWidget = (function() {
                             $('.error-list-row.file-row-' + fileId).remove();
                             $('.loading-row.file-row-' + fileId).remove();
                             updateTable();
+                            PendingFilesForm.updateSubmitForProcessing();
                         });
                         IvetlWeb.hideLoading();
                     });
