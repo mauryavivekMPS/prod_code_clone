82c464db32431a439dccc4bf623b4acc17421652
John Martin <john@lonepixel.com>
2016-04-03 13:55:26 -0700

    Added retries for seemingly valid responses that are not.

--- ivetl/connectors/doi_proxy.py
+++ ivetl/connectors/doi_proxy.py
@@ -12,22 +12,30 @@ class DoiProxyConnector(BaseConnector):
         self.tlogger = tlogger
 
     def get_hw_doi(self, doi):
+
         # use the dx resolver to get the HW version of the DOI
         url = 'http://dx.doi.org/' + doi
         headers = {'Accept': 'application/vnd.crossref.unixref+xml'}
-        r = self.get_with_retry(url, headers=headers)
-        soup = BeautifulSoup(r.content, 'xml')
-        return soup.find('doi').text
 
-    def get_with_retry(self, url, headers={}):
         attempt = 0
         success = False
-        r = None
+        hw_doi = None
         while not success and attempt < self.max_attempts:
             try:
                 r = requests.get(url, headers=headers, timeout=self.request_timeout)
                 r.raise_for_status()
-                success = True
+
+                soup = BeautifulSoup(r.content, 'xml')
+
+                doi_element = soup.find('doi')
+
+                if not doi_element:
+                    self.log('DOI Proxy return a seemlingly valid response, but no DOI is found. Trying again...')
+                    attempt += 1
+                else:
+                    hw_doi = doi_element.text
+                    success = True
+
             except requests.HTTPError as http_error:
                 if http_error.response.status_code == requests.codes.REQUEST_TIMEOUT or http_error.response.status_code == requests.codes.UNAUTHORIZED:
                     self.log("DOI Proxy API timed out. Trying again...")
@@ -44,7 +52,7 @@ class DoiProxyConnector(BaseConnector):
         if not success:
             raise MaxTriesAPIError(self.max_attempts)
 
-        return r
+        return hw_doi
 
     def log(self, message):
         if self.tlogger:
