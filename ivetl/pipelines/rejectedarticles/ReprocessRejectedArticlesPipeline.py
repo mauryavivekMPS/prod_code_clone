2b5c4f5ed4546ea2fcbab20f38f4742351b43a16
John Martin <john@lonepixel.com>
2016-04-18 13:27:32 -0700

    Fix incorrect import of deprecated BaseTask. Add missing parse task to reprocess pipeline.

--- ivetl/pipelines/rejectedarticles/ReprocessRejectedArticlesPipeline.py
+++ ivetl/pipelines/rejectedarticles/ReprocessRejectedArticlesPipeline.py
@@ -2,8 +2,15 @@ import datetime
 from celery import chain
 from ivetl.celery import app
 from ivetl.pipelines.pipeline import Pipeline
-from ivetl.pipelines.rejectedarticles import tasks
-from ivetl.pipelines.publishedarticles import tasks as published_articles_tasks
+from ivetl.pipelines.rejectedarticles.tasks.GetRejectedArticlesFromBenchpressTask import GetRejectedArticlesFromBenchPressTask
+from ivetl.pipelines.rejectedarticles.tasks.ParseBenchPressFileTask import ParseBenchPressFileTask
+from ivetl.pipelines.rejectedarticles.tasks.XREFPublishedArticleSearchTask import XREFPublishedArticleSearchTask
+from ivetl.pipelines.rejectedarticles.tasks.SelectPublishedArticleTask import SelectPublishedArticleTask
+from ivetl.pipelines.rejectedarticles.tasks.ScopusCitationLookupTask import ScopusCitationLookupTask
+from ivetl.pipelines.rejectedarticles.tasks.MendeleyLookupTask import MendeleyLookupTask
+from ivetl.pipelines.rejectedarticles.tasks.PrepareForDBInsertTask import PrepareForDBInsertTask
+from ivetl.pipelines.rejectedarticles.tasks.InsertIntoCassandraDBTask import InsertIntoCassandraDBTask
+from ivetl.pipelines.publishedarticles.tasks.CheckRejectedManuscriptTask import CheckRejectedManuscriptTask
 from ivetl.models import Publisher_Metadata
 
 
@@ -26,7 +33,7 @@ class ReprocessRejectedArticlesPipeline(Pipeline):
 
             # create work folder, signal the start of the pipeline
             work_folder = self.get_work_folder(today_label, publisher.publisher_id, product_id, pipeline_id, job_id)
-            self.on_pipeline_started(publisher.publisher_id, product_id, pipeline_id, job_id, work_folder, initiating_user_email=initiating_user_email, total_task_count=8, current_task_count=0)
+            self.on_pipeline_started(publisher.publisher_id, product_id, pipeline_id, job_id, work_folder, initiating_user_email=initiating_user_email, total_task_count=3, current_task_count=0)
 
             # construct the first task args with all of the standard bits + the list of files
             task_args = {
@@ -39,12 +46,13 @@ class ReprocessRejectedArticlesPipeline(Pipeline):
             }
 
             chain(
-                tasks.GetRejectedArticlesFromBenchpressTask.s(task_args) |
-                tasks.XREFPublishedArticleSearchTask.s() |
-                tasks.SelectPublishedArticleTask.s() |
-                tasks.ScopusCitationLookupTask.s() |
-                tasks.MendeleyLookupTask.s() |
-                tasks.PrepareForDBInsertTask.s() |
-                tasks.InsertIntoCassandraDBTask.s() |
-                published_articles_tasks.CheckRejectedManuscriptTask.s()
+                GetRejectedArticlesFromBenchPressTask.s(task_args) |
+                ParseBenchPressFileTask.s() |
+                XREFPublishedArticleSearchTask.s() |
+                SelectPublishedArticleTask.s() |
+                ScopusCitationLookupTask.s() |
+                MendeleyLookupTask.s() |
+                PrepareForDBInsertTask.s() |
+                InsertIntoCassandraDBTask.s() |
+                CheckRejectedManuscriptTask.s()
             ).delay()
