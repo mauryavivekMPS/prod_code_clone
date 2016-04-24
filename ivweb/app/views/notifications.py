6c9e1a93104f2bc46288842cafbec3cb1d7fb29f
John Martin <john@lonepixel.com>
2016-04-24 15:51:30 -0700

    Tweak dismiss link and add page filters.

--- ivweb/app/views/notifications.py
+++ ivweb/app/views/notifications.py
@@ -4,6 +4,7 @@ from operator import attrgetter
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from ivetl.models import Notification_Summary, Alert
 from ivetl.alerts import checks
 from ivweb.app.views import utils as view_utils
@@ -15,25 +16,33 @@ log = logging.getLogger(__name__)
 @login_required
 def list_notifications(request, publisher_id=None):
     if publisher_id:
-        notifications = Notification_Summary.objects.filter(publisher_id=publisher_id, dismissed=False)
+        all_notifications = Notification_Summary.objects.filter(publisher_id=publisher_id)
         alerts_by_alert_id = {a.alert_id: a for a in Alert.objects.filter(publisher_id=publisher_id)}
     else:
-        notifications = Notification_Summary.objects.filter(dismissed=False)
+        all_notifications = Notification_Summary.objects.all()
         alerts_by_alert_id = {a.alert_id: a for a in Alert.objects.all()}
 
-    for notification in notifications:
+    filter_param = request.GET.get('filter', request.COOKIES.get('notification-list-filter', 'active'))
+
+    show_dismissed = filter_param == 'dismissed'
+    filtered_notifications = all_notifications.filter(dismissed=show_dismissed)
+
+    for notification in filtered_notifications:
         setattr(notification, 'alert_name', alerts_by_alert_id[notification.alert_id].name)
 
     sort_param, sort_key, sort_descending = view_utils.get_sort_params(request, default=request.COOKIES.get('notification-list-sort', 'publisher_id'))
-    sorted_notifications = sorted(notifications, key=attrgetter(sort_key), reverse=sort_descending)
+    sorted_notifications = sorted(filtered_notifications, key=attrgetter(sort_key), reverse=sort_descending)
 
     response = render(request, 'notifications/list.html', {
         'notifications': sorted_notifications,
+        'reset_url': reverse('notifications.list') + '?sort=' + sort_param + '&filter=' + filter_param,
+        'filter_param': filter_param,
         'sort_key': sort_key,
         'sort_descending': sort_descending,
     })
 
     response.set_cookie('notification-list-sort', value=sort_param, max_age=30*24*60*60)
+    response.set_cookie('notification-list-filter', value=filter_param, max_age=30*24*60*60)
 
     return response
 
