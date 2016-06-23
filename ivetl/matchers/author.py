3c6601f106c8d9b716db5a42101f52ceef44f835
John Martin <john@lonepixel.com>
2016-06-23 14:48:12 -0700

    Revert to num authors in the uploaded title for the ratio denominator in the author match.

--- ivetl/matchers/author.py
+++ ivetl/matchers/author.py
@@ -17,15 +17,12 @@ def match_authors(rejected_last_names, crossref_last_names, tlogger=None):
     num_rejected = len(rejected_last_names)
     num_crossref = len(crossref_last_names)
 
-    if num_rejected > num_crossref:
-        largest_list_size = num_rejected
-    else:
-        largest_list_size = num_crossref
+    denom = num_rejected
 
     if num_rejected <= 0:
         ratio_match = 0.0
     else:
-        ratio_match = num_match / largest_list_size
+        ratio_match = num_match / denom
 
     author_match_score = ratio_match
 
@@ -34,11 +31,11 @@ def match_authors(rejected_last_names, crossref_last_names, tlogger=None):
         tlogger.info("Rejected Authors: " + ','.join(rejected_last_names))
         tlogger.info("CrossRef Authors: " + ','.join(crossref_last_names))
 
-    if largest_list_size >= 5 and ratio_match >= 0.70:
+    if denom >= 5 and ratio_match >= 0.70:
         authors_match = True
-    elif largest_list_size == 4 and ratio_match >= 0.75:
+    elif denom == 4 and ratio_match >= 0.75:
         authors_match = True
-    elif largest_list_size == 3 and ratio_match >= 0.66:
+    elif denom == 3 and ratio_match >= 0.66:
         authors_match = True
     elif ratio_match == 1.0:
         authors_match = True
