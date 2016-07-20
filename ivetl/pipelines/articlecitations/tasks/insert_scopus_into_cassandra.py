0e0016322c872ce24cbe72219129a8a9a6af633a
John Martin <john@lonepixel.com>
2016-07-20 12:59:47 -0500

    Add a try-except around the article_citations insert so that if the problem comes up again we have good debugging data. (The exception is re-raised so the job will fail.)

--- ivetl/pipelines/articlecitations/tasks/insert_scopus_into_cassandra.py
+++ ivetl/pipelines/articlecitations/tasks/insert_scopus_into_cassandra.py
@@ -44,30 +44,31 @@ class InsertScopusIntoCassandra(Task):
                         tlogger.info('No date for citation %s, skipping...' % citation_doi)
                         continue
 
-                    issue = data.get('issue')
-                    if type(issue) is not str:
-                        tlogger.info('Unrecognized type for issue for citation %s, skipping...' % citation_doi)
-                        continue
-
-                    Article_Citations.create(
-                        publisher_id=publisher_id,
-                        article_doi=doi,
-                        citation_doi=citation_doi,
-                        citation_scopus_id=data['scopus_id'],
-                        citation_date=citation_date,
-                        citation_first_author=data['first_author'],
-                        citation_issue=issue,
-                        citation_journal_issn=data['journal_issn'],
-                        citation_journal_title=data['journal_title'],
-                        citation_pages=data['pages'],
-                        citation_source_scopus=True,
-                        citation_title=data['title'],
-                        citation_volume=data['volume'],
-                        citation_count=1,
-                        updated=updated_date,
-                        created=updated_date,
-                        is_cohort=data['is_cohort']
-                    )
+                    # note this try-except should probably be removed when we're satisfied there are no bugs
+                    try:
+                        Article_Citations.create(
+                            publisher_id=publisher_id,
+                            article_doi=doi,
+                            citation_doi=citation_doi,
+                            citation_scopus_id=data['scopus_id'],
+                            citation_date=citation_date,
+                            citation_first_author=data['first_author'],
+                            citation_issue=data['issue'],
+                            citation_journal_issn=data['journal_issn'],
+                            citation_journal_title=data['journal_title'],
+                            citation_pages=data['pages'],
+                            citation_source_scopus=True,
+                            citation_title=data['title'],
+                            citation_volume=data['volume'],
+                            citation_count=1,
+                            updated=updated_date,
+                            created=updated_date,
+                            is_cohort=data['is_cohort']
+                        )
+                    except:
+                        tlogger.error('Exception when inserting citation for %s (%s)' % (doi, citation_doi))
+                        tlogger.error(data)
+                        raise
 
                 PublishedArticle.objects(publisher_id=publisher_id, article_doi=doi).update(citations_updated_on=updated_date)
 
