196d288c7f5907158242d1da8bc994a77b3f6b0e
John Martin <john@lonepixel.com>
2016-09-14 11:53:53 -0700

    Remove stupid debug code that short circuits service stat processing.

--- ivetl/pipelines/servicestats/tasks/insert_stats_into_cassandra.py
+++ ivetl/pipelines/servicestats/tasks/insert_stats_into_cassandra.py
@@ -4,6 +4,7 @@ from ivetl.celery import app
 from ivetl.pipelines.task import Task
 from ivetl.models import ServiceResponseTime, ServiceResponseCode
 from ivetl import utils
+from ivetl.common import common
 
 
 @app.task
@@ -25,13 +26,14 @@ class InsertStatsIntoCassandraTask(Task):
                 for line in stats_file:
                     count = self.increment_record_count(publisher_id, product_id, pipeline_id, job_id, total_count, count)
 
-                    if count > 10:
-                        continue
+                    if common.DEBUG_QUICKLY:
+                        if count > 10:
+                            continue
 
                     if line:
                         try:
                             stat_json = json.loads(line)
-                        except json.JSONDecodeError:
+                        except ValueError:
                             tlogger.info('Bad line of JSON, skipping: %s' % line)
                             continue
 
