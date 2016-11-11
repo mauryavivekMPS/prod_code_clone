2860c0d6797fc8964956b16c4d90f70f525ae3a8
John Martin <john@lonepixel.com>
2016-11-10 18:23:15 -0600

    Clean up new RABBIT URL format.

--- ivweb/wsgi_qa.py
+++ ivweb/wsgi_qa.py
@@ -30,7 +30,7 @@ os.environ["IVETL_TABLEAU_PASSWORD"] = "admin"
 os.environ["IVETL_CASSANDRA_IP"] = "10.0.0.21"
 os.environ["IVETL_EMAIL_TO_ADDRESS"] = "john@lonepixel.com"
 os.environ["IVETL_WEB_ADDRESS"] = "http://10.0.0.232"
-os.environ["IVETL_RABBITMQ_BROKER_IP"] = "10.0.1.99"
+os.environ["IVETL_RABBITMQ_BROKER_URL"] = "amqp://guest:guest@10.0.1.99:5672//"
 
 
 # This application object is used by any WSGI server configured to use this
