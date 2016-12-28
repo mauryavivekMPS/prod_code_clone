379f7d1e7a225dc15268cc12591b2bdfdf59a1ab
John Martin <john@lonepixel.com>
2016-12-28 00:16:31 -0600

    Fix up alert listing page.

--- ivetl/models/tableau_alert.py
+++ ivetl/models/tableau_alert.py
@@ -1,4 +1,5 @@
 import uuid
+import json
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
@@ -14,3 +15,31 @@ class TableauAlert(Model):
     full_emails = columns.List(columns.Text())
     enabled = columns.Boolean()
     archived = columns.Boolean(index=True)
+
+    def _generate_dict_display_string(self, d):
+        display_strings = []
+        for n, v in d.items():
+            if type(v) == list:
+                value_string = ', '.join(v)
+            else:
+                value_string = v
+            display_strings.append('%s = %s' % (n, value_string))
+        return ', '.join(display_strings)
+
+    @property
+    def params_display_string(self):
+        params = json.loads(self.alert_params)
+        return self._generate_dict_display_string(params)
+
+    @property
+    def filters_display_string(self):
+        filters = json.loads(self.alert_filters)
+        return self._generate_dict_display_string(filters)
+
+    @property
+    def has_params(self):
+        return self.alert_params and self.alert_params != '{}'
+
+    @property
+    def has_filters(self):
+        return self.alert_filters and self.alert_filters != '{}'
