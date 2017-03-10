041b3f3013533e53764a1e4ab575979ab0b47bbc
John Martin <john@lonepixel.com>
2017-03-09 20:12:43 -0800

    Change behavior of rejected article insertion to only overwrite when there is a non null/blank value. This means blanking out existing fields requires a designated string, such as "None".

--- ivetl/pipelines/rejectedarticles/tasks/insert_into_cassandra.py
+++ ivetl/pipelines/rejectedarticles/tasks/insert_into_cassandra.py
@@ -4,7 +4,6 @@ import json
 from decimal import Decimal
 from datetime import datetime
 import cassandra.util
-from cassandra.cqlengine.query import BatchQuery
 from ivetl.celery import app
 from ivetl.models import Publisher_Vizor_Updates, RejectedArticles
 from ivetl.pipelines.task import Task
@@ -38,60 +37,131 @@ class InsertIntoCassandraDBTask(Task):
                 if publisher_id == 'aaas' and (data['submitted_journal'] == 'Signaling' or data['submitted_journal'] == 'Translational Medicine'):
                     continue
 
+                try:
+                    r = RejectedArticles.objects.get(publisher_id=publisher_id, manuscript_id=manuscript_id)
+                except RejectedArticles.DoesNotExist:
+                    r = RejectedArticles(publisher_id=publisher_id, manuscript_id=manuscript_id)
+
+                r.rejected_article_id = cassandra.util.uuid_from_time(updated)
+
+                article_type = data.get('article_type')
+                if article_type:
+                    r.article_type = article_type
+
+                citation_lookup_status = data.get('citation_lookup_status')
+                if citation_lookup_status:
+                    r.citation_lookup_status = citation_lookup_status
+
+                co_authors = data.get('co_authors')
+                if co_authors:
+                    r.co_authors = co_authors
+
+                corresponding_author = data.get('corresponding_author')
+                if corresponding_author:
+                    r.corresponding_author = corresponding_author
+
+                crossref_doi = data.get('xref_doi')
+                if crossref_doi:
+                    r.crossref_doi = crossref_doi
+
+                custom = data.get('custom')
+                if custom:
+                    r.custom = custom
+
+                editor = data.get('editor')
+                if editor:
+                    r.editor = editor
+
+                first_author = data.get('first_author')
+                if first_author:
+                    r.first_author = first_author
+
+                keywords = data.get('keywords')
+                if keywords:
+                    r.keywords = keywords
+
+                manuscript_title = data.get('title')
+                if manuscript_title:
+                    r.manuscript_title = manuscript_title
+
+                published_co_authors = data.get('xref_co_authors_ln_fn')
+                if published_co_authors:
+                    r.published_co_authors = published_co_authors
+
+                published_first_author = data.get('xref_first_author')
+                if published_first_author:
+                    r.published_first_author = published_first_author
+
+                published_journal = data.get('xref_journal')
+                if published_journal:
+                    r.published_journal = published_journal
+
+                published_journal_issn = data.get('xref_journal_issn')
+                if published_journal_issn:
+                    r.published_journal_issn = published_journal_issn
+
+                published_publisher = data.get('xref_published_publisher')
+                if published_publisher:
+                    r.published_publisher = published_publisher
+
+                published_title = data.get('xref_title')
+                if published_title:
+                    r.published_title = published_title
+
+                reject_reason = data.get('reject_reason')
+                if reject_reason:
+                    r.reject_reason = reject_reason
+
+                scopus_doi_status = data.get('scopus_doi_status')
+                if scopus_doi_status:
+                    r.scopus_doi_status = scopus_doi_status
+
+                scopus_id = data.get('scopus_id')
+                if scopus_id:
+                    r.scopus_id = scopus_id
+
+                source_file_name = data.get('source_file_name')
+                if source_file_name:
+                    r.source_file_name = source_file_name
+
+                status = data.get('status')
+                if status:
+                    r.status = status
+
+                subject_category = data.get('subject_category')
+                if subject_category:
+                    r.subject_category = subject_category
+
+                submitted_journal = data.get('submitted_journal')
+                if submitted_journal:
+                    r.submitted_journal = submitted_journal
+
+                preprint_doi = data.get('preprint_doi')
+                if preprint_doi:
+                    r.preprint_doi = preprint_doi
+
                 authors_match_score = data.get('author_match_score')
                 if authors_match_score:
-                    authors_match_score = Decimal(authors_match_score)
+                    r.authors_match_score = Decimal(authors_match_score)
 
                 crossref_match_score = data.get('xref_score')
                 if crossref_match_score:
-                    crossref_match_score = Decimal(crossref_match_score)
+                    r.crossref_match_score = Decimal(crossref_match_score)
 
                 date_of_publication = data.get('xref_publishdate')
                 if date_of_publication:
-                    date_of_publication = to_datetime(date_of_publication)
+                    r.date_of_publication = to_datetime(date_of_publication)
 
                 date_of_rejection = data.get('date_of_rejection')
                 if date_of_rejection:
-                    date_of_rejection = to_datetime(date_of_rejection)
+                    r.date_of_rejection = to_datetime(date_of_rejection)
 
-                RejectedArticles.objects(
-                    publisher_id=publisher_id,
-                    manuscript_id=manuscript_id
-                ).update(
-                    rejected_article_id=cassandra.util.uuid_from_time(updated),
-                    article_type=data.get('article_type'),
-                    authors_match_score=authors_match_score,
-                    citation_lookup_status=data.get('citation_lookup_status'),
-                    co_authors=data.get('co_authors'),
-                    corresponding_author=data.get('corresponding_author'),
-                    crossref_doi=data.get('xref_doi'),
-                    crossref_match_score=crossref_match_score,
-                    custom=data.get('custom'),
-                    date_of_publication=date_of_publication,
-                    date_of_rejection=date_of_rejection,
-                    editor=data.get('editor'),
-                    first_author=data.get('first_author'),
-                    keywords=data.get('keywords'),
-                    manuscript_title=data.get('title'),
-                    published_co_authors=data.get('xref_co_authors_ln_fn'),
-                    published_first_author=data.get('xref_first_author'),
-                    published_journal=data.get('xref_journal'),
-                    published_journal_issn=data.get('xref_journal_issn'),
-                    published_publisher=data.get('xref_published_publisher'),
-                    published_title=data.get('xref_title'),
-                    reject_reason=data.get('reject_reason'),
-                    scopus_doi_status=data.get('scopus_doi_status'),
-                    scopus_id=data.get('scopus_id'),
-                    source_file_name=data.get('source_file_name'),
-                    status=data.get('status'),
-                    subject_category=data.get('subject_category'),
-                    submitted_journal=data.get('submitted_journal'),
-                    preprint_doi=data.get('preprint_doi'),
-                    mendeley_saves=int(data.get('mendeley_saves', 0)),
-                    citations=int(data.get('citations', 0)),
-                    updated=updated,
-                    created=updated,
-                )
+                r.mendeley_saves = int(data.get('mendeley_saves', 0))
+                r.citations = int(data.get('citations', 0))
+                r.updated = updated
+                r.created = updated
+
+                r.save()
 
                 tlogger.info("Inserting or updating record")
 
