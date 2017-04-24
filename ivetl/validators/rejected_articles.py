b9358ddf6b013e65869a5aa0c04fb2c39a1b2c5f
John Martin <john@lonepixel.com>
2017-04-24 13:36:01 -0500

    Removing unicode line sep filtering.

--- ivetl/validators/rejected_articles.py
+++ ivetl/validators/rejected_articles.py
@@ -10,19 +10,12 @@ class RejectedArticlesValidator(BaseValidator):
         errors = []
         total_count = 0
 
-        def reader_without_line_sep(f):
-            while True:
-                line = next(f)
-                yield line.replace('\u2028', '')
-                # yield next(f).replace('\u2028', '')
-                continue
-
         for f in files:
             file_name = os.path.basename(f)
             try:
                 with codecs.open(f, encoding='utf-8') as tsv:
                     count = 0
-                    for line in csv.reader(reader_without_line_sep(tsv), delimiter='\t', quoting=csv.QUOTE_NONE):
+                    for line in csv.reader(tsv, delimiter='\t', quoting=csv.QUOTE_NONE):
                         if line:
                             if increment_count_func:
                                 count = increment_count_func(count)
