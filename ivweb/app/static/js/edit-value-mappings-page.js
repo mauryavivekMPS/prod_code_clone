0521188e850aff54b55a512fffe405324dbe861e
John Martin <john@lonepixel.com>
2017-07-01 09:55:11 -0700

    More updates to value mapping UI.

--- ivweb/app/static/js/edit-value-mappings-page.js
+++ ivweb/app/static/js/edit-value-mappings-page.js
@@ -11,15 +11,30 @@ $.widget("custom.editvaluemappingspage", {
         $('.edit-display-value-link[data-toggle="popover"]').popover({
             html: true,
             title: 'Enter a new display value',
+            container: '#edit-value-mappings-page',
             content: function () {
-                return $(this).closest('.mapping-header').find('.edit-display-popover').html();
+                return $(this).closest('.mapping-container').find('.edit-display-popover').html();
+            }
+        }).on('show.bs.popover', function () {
+            var mappingContainer = $(this).closest('.mapping-container');
+            var canonicalValue = $(this).closest('.mapping-container').attr('canonical_value');
+            var editContainer = $('.edit-display-container[canonical_value="' + canonicalValue + '"]');
+            editContainer.find('.edit-display-textbox').val(mappingContainer.find('.display-value').text());
+        });
+
+        $('.edit-mapping-link[data-toggle="popover"]').popover({
+            html: true,
+            title: 'Choose a new mapping',
+            container: '#edit-value-mappings-page',
+            content: function () {
+                return $(this).closest('.mapping-entry-container').find('.edit-mapping-popover').html();
             }
         });
 
-        $('.mapping-container').on('click', '.popover .edit-display-button', function () {
-            var mappingContainer = $(this).closest('.mapping-container');
-            var canonicalValue = mappingContainer.attr('canonical_value');
-            var textbox = mappingContainer.find('.edit-display-textbox');
+        $('#edit-value-mappings-page').on('click', '.popover .edit-display-button', function (event) {
+            var editContainer = $(event.target).closest('.edit-display-container');
+            var canonicalValue = editContainer.attr('canonical_value');
+            var textbox = editContainer.find('.edit-display-textbox');
             var newDisplayValue = textbox.val();
 
             IvetlWeb.showLoading();
@@ -32,12 +47,38 @@ $.widget("custom.editvaluemappingspage", {
                 'csrfmiddlewaretoken': self.options.csrfToken
             };
 
+            var mappingContainer = $('.mapping-container[canonical_value="' + canonicalValue + '"]');
+
             $.post('/updatevaluedisplay/', data)
-                .always(function () {
+                .done(function () {
                     mappingContainer.find('.display-value').text(newDisplayValue);
+                    mappingContainer.find('.edit-display-value-link').popover('hide');
                     IvetlWeb.hideLoading();
                 });
 
+        });
+
+        $('.mapping-container').on('click', '.popover .edit-mapping-button', function () {
+            var mappingContainer = $(this).closest('.mapping-container');
+            var canonicalValue = mappingContainer.attr('canonical_value');
+            var textbox = mappingContainer.find('.edit-mapping-textbox');
+            // var newDisplayValue = textbox.val();
+            //
+            // IvetlWeb.showLoading();
+            //
+            // var data = {
+            //     'publisher_id': self.options.publisherId,
+            //     'mapping_type': self.options.mappingType,
+            //     'canonical_value': canonicalValue,
+            //     'display_value': newDisplayValue,
+            //     'csrfmiddlewaretoken': self.options.csrfToken
+            // };
+            //
+            // $.post('/updatevaluedisplay/', data)
+            //     .always(function () {
+            //         mappingContainer.find('.display-value').text(newDisplayValue);
+            //         IvetlWeb.hideLoading();
+            //     });
         });
     }
 });
