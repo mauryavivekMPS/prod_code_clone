79fc64bef875ab8121fa1efda00b80f54ef90689
John Martin <john@lonepixel.com>
2017-08-21 12:54:02 -0700

    Fix accidentally added create date.

--- ivetl/models/alert.py
+++ ivetl/models/alert.py
@@ -4,13 +4,12 @@ from cassandra.cqlengine.models import Model
 
 
 class Alert(Model):
-    alert_id = columns.UUID(primary_key=True, default=uuid.uuid4, index=True)
-    publisher_id = columns.Text(partition_key=True)
-    name = columns.Text()
+    publisher_id = columns.Text(primary_key=True)
     check_id = columns.Text(primary_key=True)
+    alert_id = columns.UUID(primary_key=True, default=uuid.uuid4, index=True)
+    name = columns.Text()
     check_params = columns.Text()
     filter_params = columns.Text()
     emails = columns.List(columns.Text())
     enabled = columns.Boolean()
     archived = columns.Boolean(index=True)
-    created = columns.DateTime()
