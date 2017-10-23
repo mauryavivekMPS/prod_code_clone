fdd050c1d779864877022d5aacfdc9a5c1eccb68
John Martin <john@lonepixel.com>
2017-10-23 13:42:48 -0700

    Add pagination to value mappings edit page.

--- ivweb/app/views/value_mappings.py
+++ ivweb/app/views/value_mappings.py
@@ -6,6 +6,7 @@ from django.template import loader, RequestContext
 from ivetl.value_mappings import MAPPING_TYPES, MAPPING_TYPE_PROPERTIES
 from ivetl.models import ValueMapping, ValueMappingDisplay, PublisherMetadata
 from ivetl.pipelines.publishedarticles import RefreshValueMappingsPipeline
+from ivweb.app.views import utils
 
 log = logging.getLogger(__name__)
 
@@ -38,22 +39,36 @@ def list_mappings(request):
 @login_required
 def edit(request, publisher_id, mapping_type):
     publisher = PublisherMetadata.objects.get(publisher_id=publisher_id)
-    display_values_by_canonical_value = {}
+
+    try:
+        current_page = int(request.GET.get('page', '1'))
+    except ValueError:
+        current_page = 1
+
+    mappings_per_page = 10
+    links_per_page = 5
+
     mappings_by_canonical_value = {}
-    for mapping in ValueMapping.objects.filter(publisher_id=publisher_id, mapping_type=mapping_type):
+
+    all_mappings = [m for m in ValueMapping.objects.filter(
+        publisher_id=publisher_id,
+        mapping_type=mapping_type
+    )]
+
+    all_display_values = ValueMappingDisplay.objects.filter(
+        publisher_id=publisher_id,
+        mapping_type=mapping_type,
+    )
+
+    display_values_by_canonical_value = {d.canonical_value: d.display_value for d in all_display_values}
+
+    for mapping in all_mappings:
         canonical_value = mapping.canonical_value
         if canonical_value not in mappings_by_canonical_value:
             display_value = display_values_by_canonical_value.get(canonical_value)
             if not display_value:
-                try:
-                    display_value = ValueMappingDisplay.objects.get(
-                        publisher_id=publisher_id,
-                        mapping_type=mapping_type,
-                        canonical_value=canonical_value
-                    ).display_value
-                except ValueMappingDisplay.DoesNotExist:
-                    display_value = canonical_value.title()
-                display_values_by_canonical_value[canonical_value] = display_value
+                # if it's not already in the database, default to the title-ized version of the canonical
+                display_values_by_canonical_value[canonical_value] = canonical_value.title()
 
             mappings_by_canonical_value[mapping.canonical_value] = {
                 'canonical_value': mapping.canonical_value,
@@ -63,7 +78,14 @@ def edit(request, publisher_id, mapping_type):
         else:
             mappings_by_canonical_value[mapping.canonical_value]['original_values'].append(mapping.original_value)
 
-    sorted_mappings = sorted(mappings_by_canonical_value.values(), key=lambda m: m['display_value'].lower())
+    all_sorted_mappings = sorted(mappings_by_canonical_value.values(), key=lambda m: m['display_value'].lower())
+
+    num_total_mappings = len(all_sorted_mappings)
+
+    first, last, low_page, high_page, num_pages = utils.paginate(current_page, num_total_mappings, mappings_per_page, links_per_page)
+    all_pages = range(low_page, high_page + 1)
+
+    this_page_sorted_mappings = all_sorted_mappings[first:last]
 
     canonical_choices = [{'id': k, 'name': v} for k, v in display_values_by_canonical_value.items()]
     sorted_canonical_choices = sorted(canonical_choices, key=lambda c: c['name'])
@@ -72,9 +94,14 @@ def edit(request, publisher_id, mapping_type):
         'publisher': publisher,
         'publisher_id': publisher_id,
         'mapping_type': mapping_type,
-        'mappings': sorted_mappings,
+        'mappings': this_page_sorted_mappings,
         'mapping_type_display': MAPPING_TYPE_PROPERTIES[mapping_type]['display_name'],
         'canonical_choices': sorted_canonical_choices,
+        'current_page': current_page,
+        'num_pages': num_pages,
+        'low_page': low_page,
+        'high_page': high_page,
+        'all_pages': all_pages,
     })
 
     return response
