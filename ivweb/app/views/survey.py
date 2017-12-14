d86feaa1592be4176a866a705c983f240ad66e13
John Martin <john@lonepixel.com>
2017-12-14 14:59:47 -0800

    Remove origin test from survey pages.

--- ivweb/app/views/survey.py
+++ ivweb/app/views/survey.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 SURVEYS = {
     'manager-nps-survey': 'https://www.surveymonkey.com/r/B52V9PD',
@@ -8,7 +8,7 @@ SURVEYS = {
 }
 
 
-@csrf_exempt
+@xframe_options_exempt
 def redirect(request, survey_id):
     if survey_id in SURVEYS:
         return HttpResponseRedirect(SURVEYS[survey_id] + '?source=' + request.GET.get('source', 'unknown'))
@@ -16,7 +16,7 @@ def redirect(request, survey_id):
         return HttpResponse('Unrecognized survey ID')
 
 
-@csrf_exempt
+@xframe_options_exempt
 def vizor_survey_link(request, survey_id):
     return render(request, 'survey/survey_link_for_vizors.html', {
         'survey_id': survey_id,
