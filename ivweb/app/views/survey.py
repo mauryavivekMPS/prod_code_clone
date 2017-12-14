cdf7857f3214b04873cba4210fcb49c4840796af
John Martin <john@lonepixel.com>
2017-12-14 14:42:31 -0800

    Avoid cross origin checks. Tweak link display.

--- ivweb/app/views/survey.py
+++ ivweb/app/views/survey.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 SURVEYS = {
@@ -7,6 +8,7 @@ SURVEYS = {
 }
 
 
+@csrf_exempt
 def redirect(request, survey_id):
     if survey_id in SURVEYS:
         return HttpResponseRedirect(SURVEYS[survey_id] + '?source=' + request.GET.get('source', 'unknown'))
@@ -14,6 +16,7 @@ def redirect(request, survey_id):
         return HttpResponse('Unrecognized survey ID')
 
 
+@csrf_exempt
 def vizor_survey_link(request, survey_id):
     return render(request, 'survey/survey_link_for_vizors.html', {
         'survey_id': survey_id,
