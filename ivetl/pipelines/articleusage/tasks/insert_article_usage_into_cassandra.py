1016353cf09ada92ce93a4773ef28f1dcd20dc7a
Alex Ghoth <alex@ghoth.net>
2018-11-12 14:17:15 +0000

    Made more robust

--- ivetl/pipelines/articleusage/tasks/insert_article_usage_into_cassandra.py
+++ ivetl/pipelines/articleusage/tasks/insert_article_usage_into_cassandra.py
@@ -1,5 +1,5 @@
 import os
-import csv
+import re
 import codecs
 from datetime import datetime
 from ivetl.celery import app
@@ -25,44 +25,52 @@ class InsertArticleUsageIntoCassandra(Task):
 
         count = 0
 
-        # Warning: this CQL query has a LIMIT 10000
-        publisher_dois = PublishedArticle.objects(publisher_id=publisher_id)
-
+        field_separators = '\s*\t\s*|\s*,\s*'
+        
         for f in files:
-            with open(f, encoding='utf-8') as tsv:
-                for line in csv.DictReader(tsv, delimiter='\t'):
+            with open(f, encoding='utf-8') as tsv_file:
+                # Using re.split() instead of csv.DictReader() for flexibility with field delimiters
+                header = tsv_file.readline().strip()
+                fieldnames = [fn.lower().replace(' ','') for fn in re.split(field_separators, header)]
 
+                pub_dates = {}
+
+                for row in tsv_file:
+                    columns = re.split(field_separators, row.strip())
+                    line = dict(zip(fieldnames, columns))
+                    
                     count = self.increment_record_count(publisher_id, product_id, pipeline_id, job_id, total_count, count)
 
-                    doi = line['Article DOI'].lower()
+                    doi = line['articledoi'].lower()
 
-                    try:
-                        article = publisher_dois.get(article_doi=doi)
-                    except PublishedArticle.DoesNotExist:
-                        break
-                    
-                    usage_start_date = article.date_of_publication
-                    usage_type = line['Type']
+                    if pub_dates.get(doi) is None:
+                        try:
+                            pub_dates[doi] = PublishedArticle.objects.get(publisher_id=publisher_id,
+                                                                          article_doi=doi
+                                                                         ).date_of_publication
+                        except PublishedArticle.DoesNotExist:
+                            continue
 
-                    usage_string = line['Usage Count']
-                    if usage_string:
-                        usage = int(usage_string)
-                    else:
-                        usage = 0
+                    usage_start_date = pub_dates[doi]
+                    usage_type = line['type']
+                    usage_month = datetime.strptime(str(line['usagemonth'])[:6], '%Y%m')
+                    usage_count = int(line['usagecount']) if line['usagecount'] else 0
 
-                    usage_month = datetime.strptime(line['Usage Month'], '%Y%m')
-                    
                     month_number = ( 12 * (usage_month.year - usage_start_date.year)
-                                    + usage.month - usage_start_date.month
+                                    + usage_month.month - usage_start_date.month
                                     + 1 )
-                                   
+
+                    # validate the month_number is between date_of_publication and now().month
+                    if usage_month > datetime.now() or month_number < 0:
+                        continue
+                
                     ArticleUsage.objects(
                         publisher_id=publisher_id,                        
                         article_doi=doi,
                         usage_type=usage_type,
                         month_number=month_number,
                     ).update(
-                        month_usage=usage,
+                        month_usage=usage_count,
                         usage_start_date=usage_start_date,
                     )
 
